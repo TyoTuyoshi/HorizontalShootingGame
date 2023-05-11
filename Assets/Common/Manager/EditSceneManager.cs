212925fd456b56fd966ac2f55490aa10ea231ce1
TyoTuyoshi <futureRPG@outlook.jp>
2023-05-11 17:08:42 +0900

    ListView呼び出しの実装

--- Assets/Common/Manager/EditSceneManager.cs
+++ Assets/Common/Manager/EditSceneManager.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UnityEngine.UIElements;
 using Image = UnityEngine.UI.Image;
@@ -17,24 +19,64 @@ namespace Manager
         private VisualElement[] panel;
         private Button[] btn_add;
 
-        private List<Ship> ships = new List<Ship>();
+        [SerializeField] private GameObject ShipViewer = null;
+
+        public List<Ship> ships = new List<Ship>();
         private void Start()
         {
             //シーンのフェードイン/アウト用Imageの設定
             FadeManager.Instance.SetFadeImage();
+            
             GetSgips();
+
+            var root = uiDocument.rootVisualElement;
+            //btn_add[0] = root.Q<Button>("btn_add1");
+            //btn_add[1] = root.Q<Button>("btn_add2");
+            //btn_add[2] = root.Q<Button>("btn_add3");
+
+            //追加ボタンの取得
+            
+            Button[] buttons = 
+            {
+                root.Query<Button>("btn_add1"),
+                root.Query<Button>("btn_add2"),
+                root.Query<Button>("btn_add3"),
+            };
+
+            //buttons.Select(e => e.clicked += () =>
+            //{
+            //    ShipViewer.SetActive(true);
+            //}).ToArray();
+            
+            foreach (var btn in buttons.Select((v, i) => (v, i )))
+            {
+                btn.v.clicked += () =>
+                {
+                    ShipViewer.SetActive(true);
+                };
+            }
+
+            //艦船登録
             SetShips();
             //btn_add[0] = uiDocument.rootVisualElement.Query<Button>("btn_add1");
             //Scene.SceneFadeIN("GameScene", 1.0f);
-            
+
             //フェードイン
             //キャンバス非表示を解除
             //FadeManager.Instance.FadeImage.gameObject.SetActive(true);
             //FadeManager.Instance.SceneObj.SceneFadeIN("GameScene",1.0f);
         }
-        
+
         /// <summary>
-        /// マネージャーにshipを渡す。
+        /// 艦船リストを開く
+        /// </summary>
+        private void OpenShipViewer()
+        {
+            ShipViewer.SetActive(true);
+        }
+
+        /// <summary>
+        /// マネージャーにshipを渡す
         /// </summary>
         public void SetShips()
         {
--- Assets/Common/UI/EditShip/ShipListView.cs
+++ Assets/Common/UI/EditShip/ShipListView.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Manager;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -10,10 +12,29 @@ public class ShipListView : MonoBehaviour
     private void OnEnable()
     {
         // UIDocumentコンポーネントについては次節で説明
-        var uiDocument = GetComponent<UIDocument>();
+        var root = GetComponent<UIDocument>().rootVisualElement;
         //var buttonTexts = new List<string> {"First", "Second", "Third"};
+
+        var db_ships = ShipDataBase.Instance.ShipDB;
+        var set_ships = EditSceneManager.Instance.ships;
+
+        //戻るボタン(アクティブをfalse)
+        Button btn_back = root.Q<Button>("btn_back");
+        btn_back.clicked += () =>
+        {
+            this.gameObject.SetActive(false);
+        };
         
+        //除隊ボタン
+        Button btn_remove = root.Q<Button>("btn_remove");
+        btn_remove.clicked += () =>
+        {
+            
+        };
+
+        //データベースにある艦船のうち、未だ設定されていないもの
+        List<Ship> get_ships = db_ships.Except(set_ships).ToList();
         
-        new ListViewController(uiDocument.rootVisualElement, _elementTemplate, ShipDataBase.Instance.ShipDB);
+        new ListViewController(root, _elementTemplate, get_ships);
     }
 }
